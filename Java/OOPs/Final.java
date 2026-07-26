d556b4a098c22e97822ea0f45db518d8255fc20e
madrasmonkey05 <madrascoder05@gmail.com>
2026-07-26 10:30:00 +0530

    refactored code

--- Java/OOPs/Final.java
+++ Java/OOPs/Final.java
@@ -5,24 +5,24 @@ package Java.OOPs;
 // final method cannot be overridden
 // final class cannot be inherited
 
-final class Dummy { // final class cannot be inherited
-    final int count = 0;
+// final class Dummy { // final class cannot be inherited
+// final int count = 0;
 
-    final void welcome() {
-        count = 5; // final variable cannot be reassigned
-        System.out.println("Hello, World! " + count);
-    }
-}
+// final void welcome() {
+// count = 5; // final variable cannot be reassigned
+// System.out.println("Hello, World! " + count);
+// }
+// }
 
-class Dummify extends Dummy {
-    void welcome() { // final method cannot be overridden
-        System.out.println("Hello, World!");
-    }
-}
+// class Dummify extends Dummy {
+// void welcome() { // final method cannot be overridden
+// System.out.println("Hello, World!");
+// }
+// }
 
-public class Final {
-    public static void main(String[] args) {
-        Dummy d1 = new Dummify();
-        d1.welcome();
-    }
-}
+// public class Final {
+// public static void main(String[] args) {
+// Dummy d1 = new Dummify();
+// d1.welcome();
+// }
+// }
